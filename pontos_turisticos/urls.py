3e56a8f36faa7232fc30ac713308aa7c84db0b03
Kmiokande <brunnodesouzabezerra@gmail.com>
2020-05-22 18:26:42 -0300

    Alteração do settings e urls. Alterando a região no settings; Adicionando as urls do rest;

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+from rest_framework import routers
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls')),
 ]
